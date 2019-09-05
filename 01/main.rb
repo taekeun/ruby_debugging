24e432fc2926c960441120806ee04728d5b852ff
taekeun <ytkkty@gmail.com>
2019-09-05 10:05:55 +0900

    pass 01

--- 01/main.rb
+++ 01/main.rb
@@ -9,9 +9,9 @@
 require 'pry-byebug'
 
 def format_info(data)
-  puts "Framework: #{data[1]}"
-  puts "Version: #{data[2]}"
-  puts "Workshop: #{data[3]}"
+  puts "Framework: #{data[0]}"
+  puts "Version: #{data[1]}"
+  puts "Workshop: #{data[2]}"
 end
 
 binding.pry # This is our breakpoint, the program will stop executing here
